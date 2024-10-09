71bb0442c0421f28388e609afeafe6b8e1efb1e2
Bruno S <bruno1pb13@gmail.com>
2024-10-09 02:51:16 +0000

    fix carousel

--- src/components/home/partnersCarrousel.vue
+++ src/components/home/partnersCarrousel.vue
@@ -32,9 +32,20 @@ export default {
 </script>
 
 <template>
-    <div  class="flex gap-1 sm:gap-4 justify-center">
+    <div class="sm:flex hidden gap-1 sm:gap-4 justify-center">
         <div v-for="partner in partners"  class="border border-surface-200 rounded h-auto sm:h-[10em]  w-[50em] sm:w-[13em] p-4 m-4 ">
             <img :src="partner.image" :alt="partner.name" class="w-full h-full object-contain " alt="">
         </div>
     </div>
+
+    <div  class="flex sm:hidden" >
+        <Carousel :value="partners" :numVisible="3" :numScroll="1" circular :autoplayInterval="3000">
+            <template #item="slotProps">
+                <div class=" rounded h-[10em] w-[13em] p-4 m-4 flex items-center justify-center">
+                    <img :src="slotProps.data.image" alt="" srcset="">
+                </div>
+            </template>
+        </Carousel>
+    </div>
+
 </template>
