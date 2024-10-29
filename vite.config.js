d982fa318d45b8cf2c732465aae5f6ed83a18568
Bruno S <bruno1pb13@gmail.com>
2024-10-29 15:26:51 +0000

    otimize images

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import squooshPlugin from 'vite-plugin-squoosh';
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), squooshPlugin({
+    // Specify codec options.
+    codecs: {
+      mozjpeg: { quality: 30, smoothing: 1 },
+      webp: { quality: 40 },
+      avif: { cqLevel: 20, sharpness: 1 },
+      jxl: { quality: 30 },
+      wp2: { quality: 40 },
+      oxipng: { level: 3 }
+    },
+    // Do not encode .wp2 and .webp files.
+    exclude: /.(wp2|webp)$/,
+    // Encode png to webp.
+    encodeTo: [{ from: /.(png|JPG|jpeg)$/, to: "webp" }]
+  })],
 })
