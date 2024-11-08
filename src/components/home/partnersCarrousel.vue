03d821522936af12e1683a356f45b4f3d8bc486a
Bruno S <bruno1pb13@gmail.com>
2024-11-08 12:16:23 +0000

    .

--- src/components/home/partnersCarrousel.vue
+++ src/components/home/partnersCarrousel.vue
@@ -6,13 +6,13 @@ export default {
     data() {
         return {
             partners: [
-                { image: "/assets/partners/dazEventos.jpg", name: "Daz Eventos" , link: "" },
+                { image: "/assets/partners/dazEventos.jpg", name: "Daz Eventos" , link: "https://www.instagram.com/daz_eventos/" },
                 { image: "/assets/partners/BrindesImport.png", name: "Brinder Import" },
                 { image: "/assets/partners/NB.jpg", name: "NB" },
                 { image: "/assets/partners/PontoDosBrindes.png", name: "Ponto dos brindes"},
                 { image: "/assets/partners/zwEventos.png", name: "ZW Eventos", link: "https://www.instagram.com/zweeventos/" },
                 { image: "/assets/partners/doc.png", name: "Doc" },
-                { image: "/assets/partners/danLin.jfif", name: "danLin", link: "https://www.instagram.com/danlu.buffet?igsh=bXNibWwyaGZ2bXpw" },
+                { image: "/assets/partners/danLu.jfif", name: "danLu", link: "https://www.instagram.com/danlu.buffet?igsh=bXNibWwyaGZ2bXpw" },
                 { image: "/assets/partners/rrPersonalizados.png", name: "RRPersonalizados", link: "https://wa.me/555197908970" },
                 { image: "/assets/partners/primeTravel.jpg", name: "Prime Travel"},
                 
